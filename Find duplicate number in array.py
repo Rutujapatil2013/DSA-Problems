bf5174c253eceb978999ea2c7aac021a5dceec2a
Rutujapatil2013 <75259034+Rutujapatil2013@users.noreply.github.com>
2022-07-05 13:37:10 +0530

    Update Find duplicate number in array.py

--- Find duplicate number in array.py
+++ Find duplicate number in array.py
@@ -1,4 +1,26 @@
-#FIRST SOLUTION   
+
+numRay = [0, 4, 3, 2, 7, 8, 2, 3, 1]
+arr_size = len(numRay)
+for i in range(arr_size):
+    x = numRay[i] % arr_size
+    numRay[x] = numRay[x] + arr_size
+print("The repeating elements are : ")
+for i in range(arr_size):
+    if (numRay[i] >= arr_size*2):
+        print(i, " ")
+ 
+Output: 
+The repeating elements are : 
+2 
+3
+
+Complexity Analysis: 
+Time Complexity: O(n). 
+Only two traversals are needed. So the time complexity is O(n).
+Auxiliary Space: O(1). 
+No extra space is needed, so the space complexity is constant.
+
+##############################################################################  
 # Time complexity - O(n^2)
 # Space complexity - O(1)
 code:)
